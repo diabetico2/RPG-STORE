3ebbf3cf07443bb983eeb30cfef8de5729e98062
diabetico2 <allanjosepereira18@gmail.com>
2024-11-25 07:15:49 -0300

    escreva a mensagem aqui

--- rpg-store-frontend/src/app/app.module.ts
+++ rpg-store-frontend/src/app/app.module.ts
@@ -4,19 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,  
     LoginComponent, 
     MainComponent, 
-    AppRoutingModule,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
-    RouterModule
   ],
   bootstrap: [AppComponent],
 })
